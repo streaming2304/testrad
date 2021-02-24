1b94bf18cc74d259f771eee0575b45895981b51f
artit <615021000836@gmail.rmutk.ac.th>
2021-02-24 09:50:42 +0700

    Lap

---
+++ app/controllers/SharedController.php
@@ -0,0 +1,20 @@
+<?php 
+
+/**
+ * SharedController Controller
+ * @category  Controller / Model
+ */
+class SharedController extends BaseController{
+	
+	/**
+     * post_author_option_list Model Action
+     * @return array
+     */
+	function post_author_option_list(){
+		$db = $this->GetModel();
+		$sqltext = "SELECT  DISTINCT name AS value,name AS label FROM category ORDER BY id";
+		$arr = $db->rawQuery($sqltext);
+		return $arr;
+	}
+
+}
---
+++ helpers/Menu.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+	public static $navbartopleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'category', 
+			'label' => 'Category', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'post', 
+			'label' => 'Post', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'users', 
+			'label' => 'Users', 
+			'icon' => ''
+		)
+	);
+
+	public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'category', 
+			'label' => 'Category', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'post', 
+			'label' => 'Post', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'users', 
+			'label' => 'Users', 
+			'icon' => ''
+		)
+	);
+
+	
+	
+	public static $name = array();
+
+}
